3b8de6ba00301e4069adde03a38cf6f5bb0ab630
Nisapiliang <khairunnisa.kn369@gmail.com>
2021-12-28 10:33:18 +0700

    first commit

---
+++ app/src/main/java/com/example/healthycare/kale.java
@@ -0,0 +1,14 @@
+package com.example.healthycare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class kale extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kale);
+    }
+}
